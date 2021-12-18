5dbe2ccf84a9b56947e3595f873766915110faab
tabio <tabio.github@gmail.com>
2021-12-18 20:37:51 +0900

    :books: add readme

---
+++ setup-git.sh
@@ -0,0 +1,11 @@
+# gitconfig
+cp ./gitconfig ~/.gitconfig
+
+# gitignore_global
+cp ./gitignore_global ~/.gitignore_global
+
+# commit_template
+cp ./commit_template ~/.commit_template
+
+# output message
+echo 'gitconfigのpathの設定を変更してください'
