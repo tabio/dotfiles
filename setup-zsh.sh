ee8ea7df35c30d6f651823af3a5e5bd3c7d6eb4c
tabio <tabio.github@gmail.com>
2021-12-18 21:12:22 +0900

    add zsh-autosuggestions

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,7 +1,10 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# add plugins (zsh-completions)
+# plugins
+## zsh-completions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+## zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 cp ./zshrc ~/.zshrc
