594b32b638788686419437a70c4f5e0392c7eb79
tabio <tabio.github@gmail.com>
2023-12-24 21:34:51 +0900

    fix

--- nvim/lua/config/obsidian.lua
+++ nvim/lua/config/obsidian.lua
@@ -16,5 +16,10 @@ require("obsidian").setup({
   },
   templates = {
     subdir = "templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+  },
+  attachments = {
+    img_folder = "images",
   },
 })
