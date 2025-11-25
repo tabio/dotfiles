1051fd5a2b6cb12b6fa2a6e06627359aeb85a613
tabio <a.taneoka@kakehashi.life>
2025-11-25 15:54:39 +0900

    update iterm2

--- setup-iterm2.sh
+++ setup-iterm2.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/dracula/iterm.git
 
 # output message
-echo 'iTerm2 > Preferences > Profiles > Colors Tab > Color Presets > importで↑を読み込み後選択'
+echo 'iTerm2 > Settings > Profiles > Colors Tab > Color Presets > importで↑を読み込み後選択'
+echo 'iTerm2 > Settings > Profiles > Text Tab > Fontで font-fira-code-nerd-font を選択'
